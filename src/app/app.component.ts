660765332fce650106ab148e3518eb26119dbcd0
De Isaac <deisaack@gmail.com>
2017-10-18 11:06:43 +0300

    working ngFor and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   name = '';
+  obj = {
+    name: 'isaac',
+    age: 23,
+  };
+  arr = ['abc', 'def', 'ghi'];
+  isTrue = true;
+  myName = 'De';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { ProductsComponent } from './products/products.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    ProductsComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'member',
+        component: MembersComponent,
+      },
+      {
+        path: 'product',
+        component: ProductsComponent,
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
